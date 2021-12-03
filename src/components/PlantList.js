90b1947daf9b3101808ec0bdb4722aba9f4d72a0
Roxanne Weber <marie.sankofa@gmail.com>
2021-12-03 01:44:30 -0600

    axios call working - plants rendering to plantlist

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,34 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const getPlantData = () => {
-	const data = axios.get('http://localhost:3333/plants');
-	return data;
-};
-
 export default class PlantList extends Component {
 	// add state with a property called "plants" - initialize as an empty array
 	state = {
-		plants: [
-			{
-				name: 'Peperomia Rosso',
-				id: 143,
-				scientificName: 'Peperomia caperata rosso',
-				difficulty: 'easy',
-				light: 'direct',
-				img: 'https://cdn.shopify.com/s/files/1/2781/9558/products/PEPEROMIA_ROSSO-1_800x.png?v=1587156590',
-				sizes: ['small'],
-				watering: 2,
-				description:
-					'Rosalia is a stunner with glossy green leaves accompanied by bright red undersides. Her oval shaped leaves are deeply grooved, adding depth to her figure. Flower spikes will appear with bright light, adding even more character to this absolute beaut.',
-				price: 21,
-			},
-		],
+		plants: [],
 	};
 
 	componentDidMount() {
-		getPlantData(this.state.plants).then((resp) => {
-			console.log(resp.data);
+		axios.get('http://localhost:3333/plants').then((resp) => {
+			this.setState({
+				...this.state,
+				plants: resp.data,
+			});
 		});
 	}
 
